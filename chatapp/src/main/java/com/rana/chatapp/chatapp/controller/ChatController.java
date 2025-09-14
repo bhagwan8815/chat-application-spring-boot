b3dc5a5382a3c04942c89065373fb0b9f007a043
bhagwan8815 <ranabhagwan8815@gmail.com>
2025-09-14 21:11:26 +0530

    implement chatController

--- chatapp/src/main/java/com/rana/chatapp/chatapp/controller/ChatController.java
+++ chatapp/src/main/java/com/rana/chatapp/chatapp/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,8 @@ public class ChatController {
     @Autowired
     private ChatMessageRepository chatMessageRepository;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
 
 
@@ -73,13 +76,37 @@ public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
 
 @MessageMapping("/chat.sendPrivateMessage")
 public void sendPrivateMessage(@Payload ChatMessage chatMessage , SimpMessageHeaderAccessor headerAccessor){
-    
 
+    if(userService.userExits(chatMessage.getSender() ) && userService.userExits(chatMessage.getRecepient())){
+        if(chatMessage.getTimestamp()==null){
+            chatMessage.setTimestamp(LocalDateTime.now());
+        }
 
+        if(chatMessage.getContent()==null){
+            chatMessage.setContent("");
+        }
+
+        ChatMessage savedMessage = chatMessageRepository.save(chatMessage);
+        System.out.println("Message saved successfully with id :" + savedMessage.getId());
+
+       try {
+           String recepientDestination = "/user/"+chatMessage.getRecepient()+"/queue/private";
+           System.out.println("sending message to recepient destination " + recepientDestination);
+           messagingTemplate.convertAndSend(recepientDestination , savedMessage);
+
+           String senderDestination = "/user/"+chatMessage.getSender() + "/queue/private";
+           System.out.println("sending message to sender destination " + senderDestination);
+           messagingTemplate.convertAndSend(senderDestination , savedMessage);
+       }catch (Exception e){
+           System.out.println("ERROR occured while sending the message " + e.getMessage());
+           e.printStackTrace();
+       }
+
+    }else{
+        System.out.println("Error:Sender " + chatMessage.getSender() + " or recepient " + chatMessage.getRecepient()+" does not not exits");
+    }
 
 
 }
-
-
-
 }
+//1.2
